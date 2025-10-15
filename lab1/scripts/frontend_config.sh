6e229f1591432693c17207296aca7bd93e12088c
JakMi29 <127989745+JakMi29@users.noreply.github.com>
2025-10-15 20:02:39 +0200

    Add files via upload

---
+++ lab1/scripts/frontend_config.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+echo "--- Uruchamianie skryptu db_config.sh ---"
+sudo apt-get update
+sudo apt-get install -y nodejs npm git
+sudo git clone https://github.com/JakMi29/RestaurantManagementSystem
+cd RestaurantManagementSystem
+cd frontend
+sudo npm install
+sudo VITE_BACKEND_URL='http://192.168.33.11:8080' npm run dev -- --host 0.0.0.0 --port 5173
