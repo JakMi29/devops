6e4d3050b7a212239ff96a990cc91f1cf00e51e1
JakMi29 <127989745+JakMi29@users.noreply.github.com>
2025-10-16 10:55:12 +0200

    Add files via upload

---
+++ scripts/db_config.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+apt-get update
+apt-get install -y postgresql postgresql-contrib
+PG_CONF=$(find /etc/postgresql/ -name 'postgresql.conf' -print -quit)
+PG_HBA=$(find /etc/postgresql/ -name 'pg_hba.conf' -print -quit)
+sed -i "s/^#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
+sed -i "s/^listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
+echo "host all all 192.168.56.0/24 md5" | tee -a "$PG_HBA" > /dev/null
+systemctl start postgresql
+systemctl enable postgresql
+echo "ALTER USER postgres WITH PASSWORD 'postgres';" | sudo -u postgres psql
+echo "CREATE DATABASE restaurant_management_system;" | sudo -u postgres psql
---
+++ scripts/frontend_config.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+apt-get update
+apt-get install -y nodejs npm git
+cd /vagrant/RestaurantManagementSystem
+cp -r ./frontend /opt/
+cd /opt/frontend
+npm install
+VITE_BACKEND_URL='http://192.168.56.11:8080' npm run dev -- --host 0.0.0.0 --port 5173 &
