e4388f87d7ef431044ffda27584555aaa5c83f3d
Kamil Wojcieszak <dev.kamil.wojcieszak@gmail.com>
2025-10-23 10:33:27 +0200

    remove obsolete configuration scripts for backend, database, and frontend

--- scripts/frontend_config.sh
+++
@@ -1,8 +0,0 @@
-﻿#!/bin/bash
-apt-get update
-apt-get install -y nodejs npm git
-cp -r /vagrant/frontend /opt/
-chown -R vagrant:vagrant /opt/frontend
-cd /opt/frontend
-sudo -u vagrant npm install
-sudo -u vagrant bash -c "VITE_BACKEND_URL='http://192.168.56.11:8080' npm run dev -- --host 0.0.0.0 --port 5173 &"
